c97fa313bda6c9767ddce9697e8638647fd76d22
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2025-05-31 23:33:08 +0530

    Checkpoint

--- docker/db/init-schemas.sql
+++ docker/db/init-schemas.sql
@@ -1,2 +1,17 @@
-CREATE SCHEMA IF NOT EXISTS "auth_schema";
-CREATE SCHEMA IF NOT EXISTS "identity_schema";
+-- Create all schemas needed for the application
+
+DO
+$$
+DECLARE
+    schema_list text[] := array[
+        'auth_schema',
+        'identity_schema'
+    ];
+    schema text;
+BEGIN
+    FOREACH schema IN ARRAY schema_list
+    LOOP
+        EXECUTE format('CREATE SCHEMA IF NOT EXISTS %I', schema);
+    END LOOP;
+END;
+$$ language 'plpgsql';
