f457398c1afd0b1177aa6cff3e698bb05f983c84
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2025-06-04 01:34:18 +0530

    Implemented JPA Config Shared Project

---
+++ shared/jpa-config/src/main/java/com/whispr/persistence/dao/entity/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.whispr.persistence.dao.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * Base entity class that can be extended by other entity classes.
+ *
+ * @author Arpan Mahanty
+ */
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    
+}
