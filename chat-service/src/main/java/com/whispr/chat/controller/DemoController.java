ca7e5cad3dc0e2c5eadcd0e08c3dfbb3cbbdd84f
Arpan Mahanty <arpan.mahanty.007@gmail.com>
2025-06-07 23:14:46 +0530

    Checkpoint

--- chat-service/src/main/java/com/whispr/chat/controller/DemoController.java
+++ chat-service/src/main/java/com/whispr/chat/controller/DemoController.java
@@ -1,9 +1,11 @@
 package com.whispr.chat.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/chat")
 public class DemoController {
 
     @GetMapping("/hello")
